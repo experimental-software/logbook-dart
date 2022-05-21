a7bacf468d739e211f215729b3c082f54246ce64
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-05-21 09:42:06 +0200

    feat: add layout for search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ExampleApp());
+void main() => runApp(const EngineeringLogbookApp());
 
-class ExampleApp extends StatelessWidget {
-  const ExampleApp({Key? key}) : super(key: key);
+class EngineeringLogbookApp extends StatelessWidget {
+  const EngineeringLogbookApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Hello, World!')),
-        body: const ExampleWidget(),
-      ),
+    return const MaterialApp(
+      home: Homepage(),
     );
   }
 }
 
-class ExampleWidget extends StatelessWidget {
-  const ExampleWidget({Key? key}) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final TextEditingController _searchTermController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, World!');
+    return Scaffold(
+      appBar: AppBar(title: const Text('Engineering Logbook')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add log entry',
+        child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            _buildSearchRow(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _buildSearchRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: SizedBox(
+            width: 200,
+            child: TextField(
+              controller: _searchTermController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Search log entries...',
+                suffixIcon: Icon(Icons.search),
+              ),
+              onChanged: (value) async {},
+              autofocus: true,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchTermController.dispose();
+    super.dispose();
   }
 }
