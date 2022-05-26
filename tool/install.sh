42246944ffe8558a5c4c38384a6ee96ca63f6714
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-05-26 10:55:09 +0200

    chore: simplify invocation of install script

--- tool/install.sh
+++ tool/install.sh
@@ -21,15 +21,15 @@ delete_backup
 create_backup
 
 # Compile binaries
-cd $SCRIPT_DIR/../..
+cd $SCRIPT_DIR/..
 flutter --version | grep "channel stable" || { echo "Not on stable channel." ; exit 1;  }
 flutter build linux
 cp -r ./build/linux/x64/release/bundle ~/bin/engineering_logbook
 
 # Create desktop entry
 mkdir -p ~/.local/share/ExperimentalSoftware/icons || echo ""
-cp $SCRIPT_DIR/test-tube.png ~/.local/share/ExperimentalSoftware/icons/
-cp $SCRIPT_DIR/engineering_logbook.desktop ~/.local/share/applications/
+cp $SCRIPT_DIR/resources/test-tube.png ~/.local/share/ExperimentalSoftware/icons/
+cp $SCRIPT_DIR/resources/engineering_logbook.desktop ~/.local/share/applications/
 
 # Remove backup files
 delete_backup
