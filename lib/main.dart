6af2b57b055fd0f0bc1c8f7ce8964c460a853fd0
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-06-19 20:02:57 +0200

    feat: stop page transition animations

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,39 @@ import 'pages/homepage/index.dart';
 
 void main() => runApp(const LogbookApp());
 
+class NoAnimationPageTransitionsBuilder extends PageTransitionsBuilder {
+  const NoAnimationPageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+      PageRoute<T> route,
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child,
+      ) {
+    return child;
+  }
+}
+
+const pageTransitionsTheme = PageTransitionsTheme(
+  builders: <TargetPlatform, PageTransitionsBuilder>{
+    TargetPlatform.linux: NoAnimationPageTransitionsBuilder(),
+    TargetPlatform.macOS: NoAnimationPageTransitionsBuilder(),
+  },
+);
+
 class LogbookApp extends StatelessWidget {
   const LogbookApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Homepage(),
+    return MaterialApp(
+      home: const Homepage(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+          pageTransitionsTheme: pageTransitionsTheme
+      ),
     );
   }
 }
