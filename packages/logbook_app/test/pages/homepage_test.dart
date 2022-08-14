d63deb3777dd958036308cf0c06d97e52ebecafd
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-08-14 09:09:20 +0200

    WIP

--- packages/logbook_app/test/pages/homepage_test.dart
+++ packages/logbook_app/test/pages/homepage_test.dart
@@ -3,10 +3,21 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:logbook/pages/homepage/index.dart';
 import 'package:logbook_core/logbook_core.dart';
+import 'package:get_it/get_it.dart';
 
 import '../widget_test_app.dart';
 
 void main() {
+  late _DummySearchService searchService;
+
+  setUp(() {
+    GetIt.I.registerSingleton<SearchService>(_DummySearchService());
+  });
+
+  tearDown(() async {
+    await GetIt.I.reset();
+  });
+
   testWidgets('render homepage', (tester) async {
     await tester.pumpWidget(const WidgetTestApp(Homepage()));
 
@@ -14,12 +25,27 @@ void main() {
   });
 
   testWidgets('show all log entries after opening the app', (tester) async {
-    System.baseDir = Directory('./test/test_data/single_log');
+    searchService = GetIt.I.get<SearchService>() as _DummySearchService;
+    searchService.searchResults.add(
+      LogEntry(
+        dateTime: DateTime.now(),
+        title: 'Hello, World!',
+        directory: '',
+      ),
+    );
+
     await tester.pumpWidget(const WidgetTestApp(Homepage()));
 
-    //await tester.pumpAndSettle(const Duration(milliseconds: 100), EnginePhase.sendSemanticsUpdate, const Duration(hours: 60));
     await tester.pumpAndSettle();
-    //
-    expect(find.text('Example log entry'), findsOneWidget);
+    expect(find.text('Hello, World!'), findsOneWidget);
   });
+}
+
+class _DummySearchService implements SearchService {
+  final List<LogEntry> searchResults = [];
+
+  @override
+  Future<List<LogEntry>> search(Directory _, String query) async {
+    return searchResults;
+  }
 }
