358e352267b7908b7aca37ab5cd3f9962bf9b7d6
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-08-14 10:04:25 +0200

    fix: missing get_it init

--- packages/logbook_app/lib/main.dart
+++ packages/logbook_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logbook_core/logbook_core.dart';
 
 import 'pages/homepage/index.dart';
 
-void main() => runApp(const LogbookApp());
+void main() {
+  GetIt.I.registerSingleton(SearchService());
+
+  runApp(const LogbookApp());
+}
 
 class NoAnimationPageTransitionsBuilder extends PageTransitionsBuilder {
   const NoAnimationPageTransitionsBuilder();
--- packages/logbook_cli/bin/logbook_cli.dart
+++ packages/logbook_cli/bin/logbook_cli.dart
@@ -2,8 +2,12 @@
 
 import 'package:args/command_runner.dart';
 import 'package:logbook_cli/search.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logbook_core/logbook_core.dart';
 
 void main(List<String> args) {
+  GetIt.I.registerSingleton(SearchService());
+
   CommandRunner('logbook', 'A markdown-based engineering logbook.')
     ..addCommand(SearchCommand())
     ..run(args);
