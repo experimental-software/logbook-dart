375c13002cf027003dfe6eede880ef4cc55ff6eb
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-08-14 09:18:34 +0200

    WIP

--- packages/logbook_core/lib/src/search.dart
+++ packages/logbook_core/lib/src/search.dart
@@ -6,67 +6,58 @@ import 'package:logbook_core/src/log_entry.dart';
 
 class SearchService {
   Future<List<LogEntry>> search(Directory dir, String query) async {
-    return _search(dir, query);
-  }
-}
+    final result = <LogEntry>[];
+    Stream<FileSystemEntity> entityList = dir.list(recursive: true);
+    await for (FileSystemEntity entity in entityList) {
+      if (!entity.path.endsWith('.md')) {
+        continue;
+      }
+      var parentDirectoryName = entity.parent.path;
+      final timeAndSlugMatcher = RegExp(r'.*/\d{2}.\d{2}_(.*)');
+      var timeAndSlugMatch = timeAndSlugMatcher.firstMatch(parentDirectoryName);
+      if (timeAndSlugMatch == null) {
+        continue;
+      }
+      var slug = timeAndSlugMatch.group(1)!;
 
-/// Workaround to enable [SearchService] to re-define [search] method
-/// and re-use the existing implementation.
-Future<List<LogEntry>> _search(Directory dir, String query) async {
-  return search(dir, query);
-}
-
-Future<List<LogEntry>> search(Directory dir, String query) async {
-  final result = <LogEntry>[];
-  Stream<FileSystemEntity> entityList = dir.list(recursive: true);
-  await for (FileSystemEntity entity in entityList) {
-    if (!entity.path.endsWith('.md')) {
-      continue;
-    }
-    var parentDirectoryName = entity.parent.path;
-    final timeAndSlugMatcher = RegExp(r'.*/\d{2}.\d{2}_(.*)');
-    var timeAndSlugMatch = timeAndSlugMatcher.firstMatch(parentDirectoryName);
-    if (timeAndSlugMatch == null) {
-      continue;
-    }
-    var slug = timeAndSlugMatch.group(1)!;
-
-    if (entity.path.endsWith('$slug.md')) {
-      final file = File(entity.path);
-      Stream<String> lines = file
-          .openRead()
-          .transform(utf8.decoder)
-          .transform(const LineSplitter());
-      await for (var line in lines) {
-        if (line.startsWith('# ')) {
-          final regex = RegExp(r'.*(\d{4})/(\d{2})/(\d{2})/(\d{2})\.(\d{2}).*');
-          final match = regex.firstMatch(entity.path);
-          late DateTime dateTime;
-          if (match != null) {
-            final year = int.parse(match.group(1)!);
-            final month = int.parse(match.group(2)!);
-            final day = int.parse(match.group(3)!);
-            final hour = int.parse(match.group(4)!);
-            final minute = int.parse(match.group(5)!);
-            dateTime = DateTime(year, month, day, hour, minute);
-          } else {
-            continue;
+      if (entity.path.endsWith('$slug.md')) {
+        final file = File(entity.path);
+        Stream<String> lines = file
+            .openRead()
+            .transform(utf8.decoder)
+            .transform(const LineSplitter());
+        await for (var line in lines) {
+          if (line.startsWith('# ')) {
+            final regex =
+                RegExp(r'.*(\d{4})/(\d{2})/(\d{2})/(\d{2})\.(\d{2}).*');
+            final match = regex.firstMatch(entity.path);
+            late DateTime dateTime;
+            if (match != null) {
+              final year = int.parse(match.group(1)!);
+              final month = int.parse(match.group(2)!);
+              final day = int.parse(match.group(3)!);
+              final hour = int.parse(match.group(4)!);
+              final minute = int.parse(match.group(5)!);
+              dateTime = DateTime(year, month, day, hour, minute);
+            } else {
+              continue;
+            }
+            final title = line.substring(2);
+            if (isSearchResult(title, query)) {
+              result.add(LogEntry(
+                title: title,
+                dateTime: dateTime,
+                directory: entity.parent.path,
+              ));
+            }
+            break;
           }
-          final title = line.substring(2);
-          if (isSearchResult(title, query)) {
-            result.add(LogEntry(
-              title: title,
-              dateTime: dateTime,
-              directory: entity.parent.path,
-            ));
-          }
-          break;
         }
       }
     }
+    result.sort((a, b) => b.dateTime.compareTo(a.dateTime));
+    return result;
   }
-  result.sort((a, b) => b.dateTime.compareTo(a.dateTime));
-  return result;
 }
 
 bool isSearchResult(String text, String query) {
