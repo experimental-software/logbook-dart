96c61fc42cfcf890063651dfb7c329870b3114e2
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-09-11 19:47:33 +0200

    Improve linter handling

--- packages/logbook_app/tool/test.sh
+++ packages/logbook_app/tool/test.sh
@@ -6,7 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/..
 flutter pub get
-flutter format --output=none --set-exit-if-changed .
+flutter format --fix --set-exit-if-changed .
 flutter analyze --fatal-infos
 flutter test
-# TODO if format errors, echo warning, fix format and then fail, so that re-run will pass
--- packages/logbook_cli/tool/test.sh
+++ packages/logbook_cli/tool/test.sh
@@ -6,7 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/..
 dart pub get
-dart format --output=none --set-exit-if-changed .
+dart format --fix --set-exit-if-changed .
 dart analyze --fatal-infos
 dart test
-# TODO if format errors, echo warning, fix format and then fail, so that re-run will pass
