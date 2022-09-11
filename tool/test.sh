7003d774a495141ff38b41592e0c1a8ac6897df4
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-09-11 20:43:48 +0200

    Fix upgrade test assertion

--- tool/test.sh
+++ tool/test.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 OUT=$(flutter upgrade --verify-only)
-if [[ "$OUT" == *"Flutter is already up to date"* ]]; then
+if [[ "$OUT" != *"Flutter is already up to date"* ]]; then
   echo "$OUT"
   exit 1
 fi
