461caf619cb1006bc65ea1b9f87b174ca524528f
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-09-28 07:51:16 +0200

    WIP [skip ci]

--- packages/logbook_app/lib/pages/details/index.dart
+++ packages/logbook_app/lib/pages/details/index.dart
@@ -45,28 +45,6 @@ class _DetailsPageState extends State<DetailsPage> {
     setState(() {});
   }
 
-  @deprecated
-  Future<String> _readDescriptionFromLogOrNote(Directory dir) async {
-    var files = dir.listSync();
-
-    final timeAndSlugMatcher = RegExp(r'.*/\d{2}.\d{2}_(.*)');
-    var timeAndSlugMatch = timeAndSlugMatcher.firstMatch(dir.path)!;
-    var slug = timeAndSlugMatch.group(1)!;
-
-    var result = 'not found';
-    for (var file in files) {
-      if (file.path.endsWith('$slug.md') || file.path.endsWith('index.md')) {
-        var f = File(file.path);
-        var s = f.readAsStringSync();
-        s = s.replaceFirst(RegExp(r'^#.*'), '');
-        s = s.trim();
-        result = s;
-        break;
-      }
-    }
-    return result;
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
