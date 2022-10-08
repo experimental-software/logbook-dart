26c7d018ed937526258bc33394f92acbe139cd22
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-10-08 17:18:36 +0200

    Drop latest version constraint

--- tool/test.sh
+++ tool/test.sh
@@ -4,12 +4,6 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-OUT=$(flutter upgrade --verify-only)
-if [[ "$OUT" != *"Flutter is already up to date"* ]]; then
-  echo "$OUT"
-  exit 1
-fi
-
 $SCRIPT_DIR/../packages/logbook_core/tool/lint.sh
 $SCRIPT_DIR/../packages/logbook_cli/tool/lint.sh
 $SCRIPT_DIR/../packages/logbook_app/tool/lint.sh
