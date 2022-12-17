eab22f30f89193aef1cd2f84261986a9f478dd0e
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-12-17 20:54:59 +0100

    Add more tests

--- packages/logbook_core/test/config_test.dart
+++ packages/logbook_core/test/config_test.dart
@@ -23,48 +23,92 @@ void main() {
     LogbookConfig.homeDirectory = await Directory.systemTemp.createTemp();
   });
 
-  test('create file with default values', () {
+  test('use default values without config file', () {
     var config = LogbookConfig();
 
-    var logDirectory = config.logDirectory;
-
     expect(
-        logDirectory.path, equals('${home.path}${Platform.pathSeparator}Logs'));
+      config.logDirectory.path,
+      equals('${home.path}${Platform.pathSeparator}Logs'),
+    );
+    expect(
+      config.archiveDirectory.path,
+      equals('${home.path}${Platform.pathSeparator}Archive'),
+    );
   });
 
   test('use default values for invalid yaml syntax', () async {
     await createConfig('":::::": :::');
     var config = LogbookConfig();
 
-    var logDirectory = config.logDirectory;
-
     expect(
-        logDirectory.path, equals('${home.path}${Platform.pathSeparator}Logs'));
+      config.logDirectory.path,
+      equals('${home.path}${Platform.pathSeparator}Logs'),
+    );
+    expect(
+      config.archiveDirectory.path,
+      equals('${home.path}${Platform.pathSeparator}Archive'),
+    );
+  });
+
+  group('use default values for partical config', () {
+    test('default value for log directory', () async {
+      await createConfig(
+        'archiveDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Archiv',
+      );
+      var config = LogbookConfig();
+
+      var logDirectory = config.logDirectory;
+
+      expect(logDirectory.path,
+          equals('${home.path}${Platform.pathSeparator}Logs'));
+    });
+
+    test('default value for archive directory', () async {
+      await createConfig(
+        'logDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Notizen',
+      );
+      var config = LogbookConfig();
+
+      var archiveDirectory = config.archiveDirectory;
+
+      expect(archiveDirectory.path,
+          equals('${home.path}${Platform.pathSeparator}Archive'));
+    });
   });
 
   test('configure log write directory', () async {
     await createConfig(
-        'logDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Notizen');
+      'logDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Notizen',
+    );
     var config = LogbookConfig();
 
     var logDirectory = config.logDirectory;
 
     expect(
-        logDirectory.path,
-        equals(
-            '${home.path}${Platform.pathSeparator}doc${Platform.pathSeparator}Notizen'));
+      logDirectory.path,
+      equals('${home.path}'
+          '${Platform.pathSeparator}'
+          'doc'
+          '${Platform.pathSeparator}'
+          'Notizen'),
+    );
   });
 
   test('configure log read directory', () async {
     await createConfig(
-        'archiveDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Archiv');
+      'archiveDirectory: ~${Platform.pathSeparator}doc${Platform.pathSeparator}Archiv',
+    );
     var config = LogbookConfig();
 
     var archiveDirectory = config.archiveDirectory;
 
     expect(
-        archiveDirectory.path,
-        equals(
-            '${home.path}${Platform.pathSeparator}doc${Platform.pathSeparator}Archiv'));
+      archiveDirectory.path,
+      equals('${home.path}'
+          '${Platform.pathSeparator}'
+          'doc'
+          '${Platform.pathSeparator}'
+          'Archiv'),
+    );
   });
 }
