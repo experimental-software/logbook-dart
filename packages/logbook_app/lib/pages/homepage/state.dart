6d3448467482c0eb760ee9d0d0f88b6188ad0d80
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-12-20 11:06:18 +0100

    docs: add diagram for homepage states [skip ci]

---
+++ packages/logbook_app/lib/pages/homepage/state.dart
@@ -0,0 +1,4 @@
+/// This library contains the code related to the Homepage state.
+///
+/// ![hompage state](https://experimental-software.github.io/logbook/06_runtime-view/img/pages/homepage/state.png)
+library state;
