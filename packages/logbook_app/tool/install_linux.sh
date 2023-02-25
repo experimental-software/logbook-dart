54083aada290ef0c20b91f37fd8a5f7d305b72a8
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2023-02-25 17:52:17 +0100

    build(app): simplify installation from release [skip ci]

--- packages/logbook_app/tool/install_linux.sh
+++ packages/logbook_app/tool/install_linux.sh
@@ -9,24 +9,19 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 function usage()
 {
     cat <<-END
-usage: install_appimage.sh [-h] [-f appimage_file]
+usage: install_linux.sh [-h]
 
-This script extract an AppImage of the Logbook app, places the binary files in
-/home/$(whoami)/bin/Logbook** and the icon/desktop files in
+This script download the latest Linux release of the Logbook app, places the
+binary files in /home/$(whoami)/bin/Logbook** and the icon/desktop files in
 /home/$(whoami)/.local/share/**.
 
-If no AppImage file gets provided, it downloads the AppImage from the latest
-GitHub release.
-
 optional arguments:
-  -f    Local file of the AppImage to be installed.
   -h    Show this help message and exit.
 
 dependencies:
   - Linux
   - jq
   - cURL
-  - realpath
 
 license: MIT
 END
@@ -80,47 +75,29 @@ fi
 # exit script if there is any error in any command
 set -e
 
-if [[ -f "$APPIMAGE_FILE" ]] ; then
-  APPIMAGE_FILE=$(realpath $APPIMAGE_FILE)
+# Get release details from GitHub API
+LOGBOOK_VERSION=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest | jq -r '.tag_name')
+BINARY_ARCHIVE=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest  | \
+  jq -r '.assets[] | select(.name | endswith(".tgz")) | .name')
+if [[ -z "${BINARY_ARCHIVE}" ]] ; then
+  echo "ERROR: Could not find binary archive file in release for version '${LOGBOOK_VERSION}'." >&2
+  exit 1
+fi
+
+# Check if binary files already exist
+INSTALL_DIR="/home/$(whoami)/bin/logbookapp-${LOGBOOK_VERSION}"
+if [[ -d "${INSTALL_DIR}" ]] ; then
+  echo "WARNING: Logbook app version '${LOGBOOK_VERSION}' is already installed at '${INSTALL_DIR}'." >&2
+  exit 1
 fi
 
 cd $(mktemp -d)
-echo "Using temporary directory '$(pwd)'."
+echo "Downloading release into temporary directory '$(pwd)'."
+DOWNLOAD_URL="https://github.com/experimental-software/logbook/releases/download/${LOGBOOK_VERSION}/${BINARY_ARCHIVE}"
+curl -SL ${DOWNLOAD_URL} -o ${BINARY_ARCHIVE}
 
-if [[ -f "$APPIMAGE_FILE" ]] ; then
-  cp $APPIMAGE_FILE .
-  LOGBOOK_VERSION=$(ls *.AppImage | sed 's/.*\-\(.*\)\-.*/\1/')
-
-  check_if_binary_files_already_exist $LOGBOOK_VERSION
-else
-  # Get latest version numbers from GitHub API
-  LOGBOOK_VERSION=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest | jq -r '.tag_name')
-  APPIMAGE_ASSET=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest  | \
-    jq -r '.assets[] | select(.name | endswith(".AppImage")) | .name')
-  if [[ -z "${APPIMAGE_ASSET}" ]] ; then
-    echo "ERROR: Could not find AppImage in release for version '${LOGBOOK_VERSION}'." >&2
-    exit 1
-  fi
-
-  check_if_binary_files_already_exist $LOGBOOK_VERSION
-
-  echo "Downloading AppImage release for version '${LOGBOOK_VERSION}' into temporary directory '$(pwd)'."
-  DOWNLOAD_URL="https://github.com/experimental-software/logbook/releases/download/${LOGBOOK_VERSION}/${APPIMAGE_ASSET}"
-  curl -SL ${DOWNLOAD_URL} -o ${APPIMAGE_ASSET}
-  chmod +x ${APPIMAGE_ASSET}
-fi
-
-if [[ -z "$LOGBOOK_VERSION" ]] ; then
-  echo "ERROR: Could not determine Logbook version" >&2
-  exit 1
-fi
-INSTALL_DIR="/home/$(whoami)/bin/Logbook-${LOGBOOK_VERSION}"
-
-echo "Extracting AppImage"
-./*.AppImage --appimage-extract > /dev/null
-
-echo "Copy extracted app image into installation directory '${INSTALL_DIR}'."
-cp -r squashfs-root ${INSTALL_DIR}
+mkdir -p ${INSTALL_DIR}
+tar xvf ./${BINARY_ARCHIVE} --directory ${INSTALL_DIR}
 
 # Create desktop file
 DESKTOP_FILE_SRC=${SCRIPT_DIR}/resources/logbook.desktop
@@ -129,7 +106,7 @@ if [[ -f ${DESKTOP_FILE_TARGET} ]] ; then
   rm ${DESKTOP_FILE_TARGET}
 fi
 cp ${DESKTOP_FILE_SRC} ${DESKTOP_FILE_TARGET}
-sed -i -e "s|Exec=.*|Exec=${INSTALL_DIR}/AppRun|g" ${DESKTOP_FILE_TARGET}
+sed -i -e "s|Exec=.*|Exec=${INSTALL_DIR}/logbookapp|g" ${DESKTOP_FILE_TARGET}
 chmod +x ${DESKTOP_FILE_TARGET}
 
 # Success message
