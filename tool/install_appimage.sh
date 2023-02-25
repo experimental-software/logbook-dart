94e3c497e7b20fa0ecbdd104d1a3ce183586d749
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2023-02-25 16:00:42 +0100

    Improve script stability [skip ci]

--- tool/install_appimage.sh
+++ tool/install_appimage.sh
@@ -9,9 +9,9 @@ function usage()
     cat <<-END
 usage: install_appimage.sh [-h]
 
-This script downloads the latest published AppImage of the Logbook
-app, extracts it, places the binary files in /home/$(whoami)/bin, and the Icon
-and Desktop files in /home/$(whoami)/.local/share/**.
+This script download the latest an AppImage of the Logbook app, places the
+binary files in /home/$(whoami)/bin/Logbook** and the icon/desktop files in
+/home/$(whoami)/.local/share/**.
 
 optional arguments:
   -h    Show this help message and exit.
@@ -41,7 +41,7 @@ shift $((OPTIND-1))
 
 which jq > /dev/null || { echo "ERROR: jq not installed" ; exit 1 ; }
 
-if [[ ! "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ ! "${OSTYPE}" == "linux-gnu"* ]]; then
   echo "ERROR: This script only work on Linux" >&2
   exit 1
 fi
@@ -55,8 +55,12 @@ set -e
 
 # Get latest version numbers from GitHub API
 LOGBOOK_VERSION=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest | jq -r '.tag_name')
-APPIMAGE_ASSET=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest  | jq -r '.assets[0].name')
-# TODO: Support potential availability of multiple assests
+APPIMAGE_ASSET=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest  | \
+  jq -r '.assets[] | select(.name | endswith(".AppImage")) | .name')
+if [[ -z "${APPIMAGE_ASSET}" ]] ; then
+  echo "ERROR: Could not find AppImage in release for version '${LOGBOOK_VERSION}'." >&2
+  exit 1
+fi
 
 # Check if binary files already exist
 INSTALL_DIR="/home/$(whoami)/bin/Logbook-${LOGBOOK_VERSION}"
@@ -65,15 +69,16 @@ if [[ -d "${INSTALL_DIR}" ]] ; then
   exit 1
 fi
 
-# Download appimage release
+echo "Downloading AppImage release for version '${LOGBOOK_VERSION}' into temporary directory '$(pwd)'."
 cd $(mktemp -d)
-echo "Downloading AppImage release into temporary directory $(pwd)"
 DOWNLOAD_URL="https://github.com/experimental-software/logbook/releases/download/${LOGBOOK_VERSION}/${APPIMAGE_ASSET}"
 curl -SL ${DOWNLOAD_URL} -o ${APPIMAGE_ASSET}
 chmod +x ${APPIMAGE_ASSET}
-./${APPIMAGE_ASSET} --appimage-extract
 
-# Copy extracted app image into installation directory
+echo "Extracting AppImage '${APPIMAGE_ASSET}'"
+./${APPIMAGE_ASSET} --appimage-extract > /dev/null
+
+echo "Copy extracted app image into installation directory '${INSTALL_DIR}'."
 cp -r squashfs-root ${INSTALL_DIR}
 
 # Normalize binary path in Desktop file
@@ -82,11 +87,11 @@ sed -i -e "s|Exec=.*|Exec=${INSTALL_DIR}/AppRun|g" ${DESKTOP_FILE_SRC}
 
 # Enable start of Logbook app from launcher
 DESKTOP_FILE_TARGET="/home/$(whoami)/.local/share/applications/logbook.desktop"
-if [[ -f $DESKTOP_FILE_TARGET ]] ; then
-  rm $DESKTOP_FILE_TARGET
+if [[ -f ${DESKTOP_FILE_TARGET} ]] ; then
+  rm ${DESKTOP_FILE_TARGET}
 fi
-cp $DESKTOP_FILE_SRC $DESKTOP_FILE_TARGET
-chmod +x $DESKTOP_FILE_TARGET
+cp ${DESKTOP_FILE_SRC} ${DESKTOP_FILE_TARGET}
+chmod +x ${DESKTOP_FILE_TARGET}
 
 # Success message
 echo
@@ -96,5 +101,5 @@ echo "-------------------------------------------------------------"
 echo
 echo "${INSTALL_DIR}"
 echo
-cat $DESKTOP_FILE_TARGET
+cat ${DESKTOP_FILE_TARGET}
 echo
