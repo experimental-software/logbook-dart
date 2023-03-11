033b75069b0921508a05858949e58ce6618ebcff
Jan Mewes <jan.mewes@experimental-software.com>
2023-03-11 11:55:59 +0100

    build(app): add flag for specific version for linux installer [skip ci]

--- packages/logbook_app/tool/install_linux.sh
+++ packages/logbook_app/tool/install_linux.sh
@@ -9,13 +9,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 function usage()
 {
     cat <<-END
-usage: install_linux.sh [-h]
+usage: install_linux.sh [-v] [-h]
 
 This script download the latest Linux release of the Logbook app, places the
 binary files in /home/$(whoami)/bin/Logbook** and the icon/desktop files in
 /home/$(whoami)/.local/share/**.
 
 optional arguments:
+  -v    Logbook version, e.g. 0.3.10
   -h    Show this help message and exit.
 
 dependencies:
@@ -27,8 +28,12 @@ license: MIT
 END
 }
 
-while getopts "h" o; do
+while getopts "h v:" o; do
+  
   case "${o}" in
+    v)
+      LOGBOOK_VERSION=${OPTARG}
+      ;;
     h | *)
       usage
       exit 0
@@ -56,23 +61,35 @@ fi
 set -e
 
 # Get release details from GitHub API
-LOGBOOK_VERSION=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest | jq -r '.tag_name')
-BINARY_ARCHIVE=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest  | \
-  jq -r '.assets[] | select(.name | endswith(".tar.gz")) | .name')
-if [[ -z "${BINARY_ARCHIVE}" ]] ; then
-  echo "ERROR: Could not find binary archive file in release for version '${LOGBOOK_VERSION}'." >&2
-  exit 1
+if [[ -z "${LOGBOOK_VERSION}" ]] ; then
+  LATEST_RELEASE_INFO=$(curl -s https://api.github.com/repos/experimental-software/logbook/releases/latest)
+  if [[ $LATEST_RELEASE_INFO == *"API rate limit exceeded"* ]]; then
+    echo "ERROR: GitHub API rate limit exceeded. Get the latest version number from" >&2
+    echo "https://github.com/experimental-software/logbook/releases/latest and call" >&2
+    echo "the script again with the -v flag." >&2
+    exit 1
+  else
+    LOGBOOK_VERSION=$(echo "${LATEST_RELEASE_INFO}" | jq -r '.tag_name')
+    BINARY_ARCHIVE=$(echo "${LATEST_RELEASE_INFO}" | \
+      jq -r '.assets[] | select(.name | endswith(".tar.gz")) | .name')
+    if [[ -z "${BINARY_ARCHIVE}" ]] ; then
+      echo "ERROR: Could not find binary archive file in release for version '${LOGBOOK_VERSION}'." >&2
+      exit 1
+    fi
+  fi
+else
+  BINARY_ARCHIVE="logbookapp-${LOGBOOK_VERSION}-linux-x86_64.tar.gz "
 fi
 
 # Check if binary files already exist
 INSTALL_DIR="/home/$(whoami)/bin/logbookapp-${LOGBOOK_VERSION}"
 if [[ -d "${INSTALL_DIR}" ]] ; then
   echo "WARNING: Logbook app version '${LOGBOOK_VERSION}' is already installed at '${INSTALL_DIR}'." >&2
-  exit 1
+  exit 0
 fi
 
 cd $(mktemp -d)
-echo "Downloading release into temporary directory '$(pwd)'."
+echo "Downloading release '${LOGBOOK_VERSION}' into temporary directory '$(pwd)'."
 DOWNLOAD_URL="https://github.com/experimental-software/logbook/releases/download/${LOGBOOK_VERSION}/${BINARY_ARCHIVE}"
 curl -SL ${DOWNLOAD_URL} -o ${BINARY_ARCHIVE}
 
